02a6763a55e4c55b11628be3bbcb349bbbdf8edd
edenjun <jun.a.deng@ericsson.com>
2020-12-18 17:14:56 +0800

    regular mm update

--- freeplane/DNEW Concept.mm
+++ freeplane/DNEW Concept.mm
@@ -1,9 +1,9 @@
 <map version="freeplane 1.8.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="DNEW Concept" FOLDED="false" ID="ID_1078077666" CREATED="1602555961549" MODIFIED="1602555998011" STYLE="oval">
+<node TEXT="DNEW Concept/&#xa;STAR Project" FOLDED="false" ID="ID_1078077666" CREATED="1602555961549" MODIFIED="1608255680714" STYLE="oval">
 <font SIZE="18"/>
 <hook NAME="MapStyle">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -63,9 +63,41 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="New Concepts Program" POSITION="right" ID="ID_1649933696" CREATED="1602556265496" MODIFIED="1602556287585">
-<edge COLOR="#ff0000"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      目前我们知道的，公司内部有两个比较前沿的研究小组：
+    </p>
+    <ul>
+      <li>
+        <b>DNEW Concept小组 </b>
+      </li>
+      <li>
+        <b>STAR Project小组</b>
+      </li>
+    </ul>
+    <p>
+      
+    </p>
+    <p>
+      这里的小结的内容是从DNEW Concept小组的报告中获取的。
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="DNEW Concept" POSITION="left" ID="ID_1789683672" CREATED="1608256421959" MODIFIED="1608256429112">
+<edge COLOR="#ff00ff"/>
+<node TEXT="New Concepts Program" ID="ID_1649933696" CREATED="1602556265496" MODIFIED="1608256441450">
 <node TEXT="Early Phases Focus Area RAN Deployment" ID="ID_617067518" CREATED="1602556394589" MODIFIED="1602556402638">
 <node TEXT="Indoor vs outdoor classification of smartphone users" ID="ID_1204872569" CREATED="1602557741064" MODIFIED="1602557743518"/>
 <node TEXT="High Band (mmW) network impacts from modern repeaters" ID="ID_1545651531" CREATED="1602557751540" MODIFIED="1602557753030"/>
@@ -89,11 +121,8 @@
 </node>
 </node>
 </node>
-<node TEXT="S&amp;T Architecture Program" POSITION="left" ID="ID_1690241532" CREATED="1602556300420" MODIFIED="1602556313152">
-<edge COLOR="#0000ff"/>
-</node>
-<node TEXT="Technology and Architecture Orchestration(TAO) Program" POSITION="right" ID="ID_418729103" CREATED="1602556325118" MODIFIED="1602556346752">
-<edge COLOR="#00ff00"/>
+<node TEXT="S&amp;T Architecture Program" ID="ID_1690241532" CREATED="1602556300420" MODIFIED="1608256443876"/>
+<node TEXT="Technology and Architecture Orchestration(TAO) Program" ID="ID_418729103" CREATED="1602556325118" MODIFIED="1608256446053">
 <node TEXT="Early Phases Focus Area Automation of RAN" ID="ID_1041421983" CREATED="1602556507078" MODIFIED="1602556513512"/>
 <node TEXT="Early Phases Focus Area Fronthaul Solutions" ID="ID_1553556534" CREATED="1602556522310" MODIFIED="1602556529096"/>
 <node TEXT="Early Phases Focus Area RAN Security" ID="ID_1447467243" CREATED="1602556541381" MODIFIED="1602556547918"/>
@@ -107,6 +136,41 @@
 <node TEXT="Street &amp; Indoor Solutions" ID="ID_158192018" CREATED="1602556673637" MODIFIED="1602556678471"/>
 <node TEXT="Transport, Synchronization and Microwave" ID="ID_1911529241" CREATED="1602556687574" MODIFIED="1602556693088"/>
 <node TEXT="Security" ID="ID_114775751" CREATED="1602556708086" MODIFIED="1602556712632"/>
+</node>
+</node>
+<node TEXT="STAR Project" POSITION="right" ID="ID_1221820357" CREATED="1608256451126" MODIFIED="1608256457435">
+<edge COLOR="#00ffff"/>
+<node TEXT="Target Followup" ID="ID_462273589" CREATED="1608259518443" MODIFIED="1608259525178">
+<node TEXT="R17 concept development" ID="ID_1068144763" CREATED="1608256468516" MODIFIED="1608256477754"/>
+<node TEXT="R18 concept development" ID="ID_1916320139" CREATED="1608256477937" MODIFIED="1608256485156"/>
+<node TEXT="Technical leadership" ID="ID_945306701" CREATED="1608256487240" MODIFIED="1608256493207"/>
+<node TEXT="IPR" ID="ID_1229499074" CREATED="1608256923020" MODIFIED="1608256928938"/>
+<node TEXT="R16 product support" ID="ID_1749426389" CREATED="1608257010586" MODIFIED="1608257019857"/>
+<node TEXT="Spectrum Related" ID="ID_1740341034" CREATED="1608257155989" MODIFIED="1608257163072"/>
+<node TEXT="External communication" ID="ID_595224454" CREATED="1608257163925" MODIFIED="1608257170719"/>
+<node TEXT="WoW" ID="ID_1842845020" CREATED="1608257831149" MODIFIED="1608257834783"/>
+</node>
+<node TEXT="Technical Progress" ID="ID_849096485" CREATED="1608259551077" MODIFIED="1608259560338">
+<node TEXT="PHY and unlicensed team" ID="ID_1274712913" CREATED="1608259578603" MODIFIED="1608259587499">
+<node TEXT="NR-U(60G)" ID="ID_1920731565" CREATED="1608259739972" MODIFIED="1608259768805"/>
+<node TEXT="Rel16(5-6GHz)" ID="ID_1542446266" CREATED="1608259776999" MODIFIED="1608259787017"/>
+<node TEXT="Rel17(60GHz)" ID="ID_31688227" CREATED="1608259787360" MODIFIED="1608259797588"/>
+<node TEXT="MulteFire" ID="ID_678153599" CREATED="1608259812737" MODIFIED="1608259816298"/>
+<node TEXT="ETSI BRAN" ID="ID_175257085" CREATED="1608259843702" MODIFIED="1608259850401"/>
+<node TEXT="Dynamic Spectrum Sharing" ID="ID_744899988" CREATED="1608259866336" MODIFIED="1608259875452"/>
+<node TEXT="UE energy efficiency" ID="ID_443760661" CREATED="1608260043429" MODIFIED="1608260060204"/>
+<node TEXT="coverage enhancement" ID="ID_530850072" CREATED="1608260073620" MODIFIED="1608260084120"/>
+</node>
+<node TEXT="Multi-antenna focused team" ID="ID_1633324765" CREATED="1608259596727" MODIFIED="1608259605202"/>
+<node TEXT="HL-focused team" ID="ID_696526993" CREATED="1608259607958" MODIFIED="1608259614910"/>
+<node TEXT="Positioning-focused team" ID="ID_367723587" CREATED="1608259617111" MODIFIED="1608259622773"/>
+<node TEXT="mMTC-focused team" ID="ID_1560503018" CREATED="1608259624378" MODIFIED="1608259630656"/>
+<node TEXT="Verticals" ID="ID_1608980849" CREATED="1608259633142" MODIFIED="1608259642963"/>
+<node TEXT="Spectrum-focused team" ID="ID_78716584" CREATED="1608259645001" MODIFIED="1608259653859"/>
+<node TEXT="ORAN and RAN3-focused team" ID="ID_1178373074" CREATED="1608259657942" MODIFIED="1608259666465"/>
+<node TEXT="RAN4-focused team" ID="ID_938986852" CREATED="1608259671678" MODIFIED="1608259676883"/>
+</node>
+<node TEXT="Reports/Presentations/Papers" ID="ID_1148525187" CREATED="1608259691569" MODIFIED="1608259707781"/>
 </node>
 </node>
 </map>
